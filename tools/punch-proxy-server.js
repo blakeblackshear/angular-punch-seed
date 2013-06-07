3b97cc3b21d672d6a3ea3bbb3f11bd546e7061cd
Blake Blackshear <bblackshear@nextgxdx.com>
2013-06-07 08:45:33 -0500

    adding proxy server middleware until changes are incorporated more elegantly into punch

---
+++ tools/punch-proxy-server.js
@@ -0,0 +1,59 @@
+var connect = require("connect");
+var accept  = require("http-accept");
+var httpProxy = require("http-proxy");
+
+var page_server = require("../node_modules/punch/lib/page_server.js");
+
+module.exports = {
+
+  config: {},
+
+	startServer: function(config){
+	var self = this;
+
+	// extend the default configuration
+	self.config = config;
+
+		var app = connect.call();
+
+		var proxy = new httpProxy.RoutingProxy({
+			changeOrigin: true
+		});
+		
+		var endpoint = {
+			host: config.proxy.host,
+			port: config.proxy.port
+		};
+
+		// log requests
+		app.use( connect.logger("short") );
+
+		// set access types in request object
+		app.use(accept);
+
+		// parse cookies
+		app.use( connect.cookieParser() );
+
+		// compress responses to GZip/Deflate
+		app.use( connect.compress() );
+
+		app.use( function(req, res, next){
+			if(req.url.indexOf(config.proxy.path) === 0){
+				proxy.proxyRequest(req, res, endpoint);
+			}
+			else{
+				return next();
+			}
+		});
+
+		// there will be 3 main middlewares
+		// page server - servers rendered or cached pages
+		// api server - gives access to puch template & content API (/api)
+		// editor - loads the pages with editing capablities (/editor)
+		app.use( page_server.setup(self.config) );
+
+		app.listen(self.config.server.port);
+
+		console.log("Started Punch server on port %s", self.config.server.port);
+	}
+};
